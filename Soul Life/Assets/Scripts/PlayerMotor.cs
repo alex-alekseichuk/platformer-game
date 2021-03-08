27e98efebd70628bc131b5803a27b245685fdfe7
4ae4eKru <78693525+4ae4eKru@users.noreply.github.com>
2021-03-08 21:18:33 +0500

    Перенос в новый репозиторий

---
+++ Soul Life/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator movePlayer;
+    
+    [SerializeField] private GameObject target;
+
+    [SerializeField] private GameObject enterExit;
+    [SerializeField] private string sceneToLoad;
+
+    [SerializeField] private float maxRange;
+    [SerializeField] private float speed;
+    
+    private Vector3 distance;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneTransition.SceneManeger("Menu");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        distance = target.transform.position - player.transform.position;
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            movePlayer.SetBool("Walk_F", true);
+            movePlayer.SetBool("Idle", false);
+            movePlayer.SetBool("Walk_B", false);
+            movePlayer.speed = speed;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            movePlayer.SetBool("Walk_B", true);
+            movePlayer.SetBool("Walk_F", false);
+            movePlayer.SetBool("Idle", false);
+            movePlayer.speed = speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += Vector3.left * Input.GetAxisRaw("Horizontal") * speed;
+        }
+        else if( Input.GetKey(KeyCode.D))
+        {
+            player.transform.position -= Vector3.left * Input.GetAxisRaw("Horizontal") * speed;
+        }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            movePlayer.SetBool("Idle", true);
+            movePlayer.SetBool("Walk_B", false);
+            movePlayer.SetBool("Walk_F", false);
+        }
+        
+        if (distance.sqrMagnitude < maxRange * maxRange)
+        {
+            enterExit.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                
+                print("E pressed!");
+                SceneTransition.SceneManeger(sceneToLoad);
+            }
+        }
+        else
+        {
+            enterExit.SetActive(false);
+        }
+    }
+}
---
+++ Soul Life/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,66 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+    public TextMeshProUGUI loadingPercentage;
+    public Image loadingProgressBar;
+    
+    public static SceneTransition instance;
+    private static bool shoodPlayAnimation;
+
+    private Animator componentAnimator;
+    private AsyncOperation loadingSceneOperation;
+
+    public void ManagerScene(string stringName)
+    {
+        SceneManeger(stringName);
+    }
+    public static void SceneManeger(string sceneName)
+    {
+        instance.componentAnimator.SetTrigger("Scene Start");
+
+        instance.loadingSceneOperation =  SceneManager.LoadSceneAsync(sceneName);
+        instance.loadingSceneOperation.allowSceneActivation = false;
+        
+    }
+    
+    private void Start()
+    {
+        instance = this;
+
+        componentAnimator = GetComponent<Animator>();
+        
+        
+        componentAnimator.SetTrigger("Scene Idle");
+        if(shoodPlayAnimation) componentAnimator.SetTrigger("Scene End");
+    }
+
+    public void OnAnimationOver()
+    {
+        print("Animation over!");
+        loadingSceneOperation.allowSceneActivation = true;
+        shoodPlayAnimation = true;
+    }
+
+    private void Update()
+    {
+        if (loadingSceneOperation != null)
+        {
+            loadingPercentage.text = Mathf.RoundToInt(loadingSceneOperation.progress * 100) + "%";
+            loadingProgressBar.fillAmount = loadingSceneOperation.progress;   
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadSceneAsync("Menu");
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            SceneManager.LoadSceneAsync("worldScene");
+        }
+    }
+}
